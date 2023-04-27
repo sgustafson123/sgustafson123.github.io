a1115e8ee902dacc9495acaaaa91fc56025d3252
Sonja Gustafson <sgustaf15@gmail.com>
2023-04-27 03:34:43 -0500

    add visitor log

---
+++ imageslider.js
@@ -0,0 +1,8 @@
+let visitorForm = document.getElementById("visitorForm");
+
+visitorForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let name = document.getElementById("Name");
+  console.log(name);
+});

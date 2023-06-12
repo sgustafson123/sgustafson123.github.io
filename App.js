1486a1958339e1b97ef9461299e23fba9c998273
Sonja Gustafson <124421321+sgustafson123@users.noreply.github.com>
2023-06-11 23:07:37 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,41 +1,58 @@
-import React from "react";
-// TODO: import useFormik from formik library
-import { useFormik } from "formik";
-function App() {
-  // TODO: add a const called formik assigned to useFormik()
-  const formik = useFormik({
-    initialValues:{
-      email: '',
-      password: ''
-    },
-    onSubmit = values =>{
-      console.log('form:',values)
-    },
-    validate: values =>{
-      let errors = {};
-      if(!values.emailField)errors.emailField='Field Required';
-      if(!values.emailField)errors.emailField='Username should be an email'
-      if(!values.password)errors.password='Field Required';
-      return errors;
-      alert("Login Successful")
-    })
-  };  
+import "./App.css";
+import { useState } from "react";
+import { Item } from "./Item";
+import { Form } from "./Form";
+
+const App = () => {
+  const [value, setValue] = useState(" ");
+  const [tasks, setTasks] = useState([
+    { id: 1, task: "Walk the dog", isCompleted: false },
+    { id: 2, task: "Grocery shopping", isCompleted: false },
+    { id: 3, task: "Make the bed", isCompleted: false },
+    { id: 4, task: "Cook pasta", isCompleted: false },
+  ]);
+  const handleClick = () => {
+    setTasks([
+      ...tasks,
+      { id: tasks.length + 1, task: value, isCompleted: false },
+    ]);
+    setValue(" ");
+  };
+  const handleIsComplete = (id) => {
+    const foundTask = tasks.find((task) => task.id === id);
+    foundTask.isCompleted = !foundTask.isCompleted;
+
+    const newTasks = tasks.map((task) => (task.id === id ? foundTask : task));
+
+    setTasks(newTasks);
+  };
+
+  const handleDelete = (id) => {
+    const updatedTasks = tasks.filter((task) => task.id !== id);
+
+    setTasks(updatedTasks);
+  };
+
   return (
-    <div>
-      <p>
-        The app is ready! You can proceed with the task instructions. TODO:
-        build you form here.
-      </p>
-      <form onSubmit={formik.handleSubmit}>
-        <div>Email</div>
-        <input name='emailField' type='text' onChange={formik.handleChange} value={formik.values.emailField}/>
-        {formik.errors.emailField?<div style={{color:'red'}}>{formik.errors.emailField}</div>:null}
-        <div>Password</div>
-        <input name='pswField' type='text' onChange={formik.handleChange} value={formik.values.pswField}/>
-        {formik.errors.pswField?<div style={{color:'red'}}>{formik.errors.pswField}</div>:null}
-        <button type='submitBtn'>Submit</button>
-      </form>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <h1> To-Do List</h1>
+        </header>
+        {tasks.map((item) => (
+          <Item
+            key={item.id}
+            id={item.id}
+            task={item.task}
+            isCompleted={item.isCompleted}
+            handleIsComplete={handleIsComplete}
+            handleDelete={handleDelete}
+          />
+        ))}
+      </div>
+      <Form value={value} setValue={setValue} handleClick={handleClick} />
+    </>
   );
+};
 
 export default App;
